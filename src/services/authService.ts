90fbbeeef9aa359e23086aa695a75086970c081f
fbotalla <botalla.fabrizio@gmail.com>
2026-05-07 23:36:43 +0200

    goo

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -39,7 +39,7 @@ export class AuthService {
         iat: Math.floor(Date.now() / 1000),
       },
       JWT_SECRET!,
-      { expiresIn: "24h" }
+      { expiresIn: "30d" }
     );
 
     return {
